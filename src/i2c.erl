c31133d3b71739d8f58457776a66076cf1373bc1
Jean Parpaillon <jean.parpaillon@free.fr>
2016-05-23 20:07:09 +0200

    Allow to load driver when priv_dir does not work

--- src/i2c.erl
+++ src/i2c.erl
@@ -7,40 +7,49 @@
 -define(CMD_OPEN, 4).
 
 start_link(Device) ->
-	Dir = code:priv_dir(lcd_app),
-	case erl_ddll:load_driver(Dir, i2c_drv) of
-		ok -> ok;
-		{error, already_loaded} -> ok;
-		{error, Message} -> exit(erl_ddll:format_error(Message))
-	end,
-	gen_server:start_link({local, ?MODULE}, ?MODULE, [Device], []).
+    _ = application:load(lcd_app),
+    case erl_ddll:try_load(priv_dir(), i2c_drv, [{driver_options, [kill_ports]}]) of
+	ok -> ok;
+	{error, already_loaded} -> ok;
+	{error, Message} -> exit(erl_ddll:format_error(Message))
+    end,
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [Device], []).
 
 init(Device) ->
-	Port = open_port({spawn_driver, i2c_drv}, [use_stdio, binary]),
-	port_control(Port, ?CMD_OPEN, Device),
-	{ ok, Port }.
+    Port = open_port({spawn_driver, i2c_drv}, [use_stdio, binary]),
+    port_control(Port, ?CMD_OPEN, Device),
+    { ok, Port }.
 
 handle_call({write_list, Register, Value}, _From, State) ->
-	port_control(State, ?CMD_WRITE_BLOCK_DATA, [Register, Value]),
-	{reply, ok, State}.
+    port_control(State, ?CMD_WRITE_BLOCK_DATA, [Register, Value]),
+    {reply, ok, State}.
 
 handle_cast(_Request, State) ->
-	{noreply, State}.
+    {noreply, State}.
 
 handle_info(_Info, State) ->
-	{noreply, State}.
+    {noreply, State}.
 
 terminate(_Reason, _State) ->
-	ok.
+    ok.
 
 code_change(_OldVsn, State, _Extra) ->
-	{ok, State}.	
+    {ok, State}.	
 
 write_list(Register, Value) ->
-	gen_server:call(?MODULE, {write_list, Register, Value}).
+    gen_server:call(?MODULE, {write_list, Register, Value}).
 
 word_to_byte_array(Word) ->
-	[Word band 16#ff, Word bsr 8].
+    [Word band 16#ff, Word bsr 8].
 
 write_word(Register, W) ->
-	write_list(Register, word_to_byte_array(W)).
+    write_list(Register, word_to_byte_array(W)).
+
+
+priv_dir() ->
+    case code:priv_dir(lcd_app) of
+	{error, bad_name} ->
+	    filename:join([filename:dirname(code:which(?MODULE)), "..", "priv"]);
+	Path ->
+	    Path
+    end.
